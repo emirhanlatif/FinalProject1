9196a902ba09fc772e9408fe60f3270fa50a115a
Emirhan Latif <emirhanlatif@outlook.com>
2021-02-09 11:53:56 +0300

    9 Şubat 2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,14 +1,14 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //class : referans tip
     //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
-    //new() : new'lenebilir  
+    //new() : new'lenebilir olmalı
     public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
